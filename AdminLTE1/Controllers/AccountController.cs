4b2204f05d45e142ba4f5098269814736a977810
RISKICOS <RISKICOS@DESKTOP-MDE8QRD>
2021-05-07 19:08:56 -0700

    update login

---
+++ AdminLTE1/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdminLTE1.Models;
+
+namespace AdminLTE1.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        DB_FINGERDataContext dbctx = new DB_FINGERDataContext();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
