d150fcd6024a12bb0efc577cc9dee900b3ac0f69
RISKICOS <RISKICOS@DESKTOP-MDE8QRD>
2021-05-07 21:47:16 -0700

    OKEEEE

--- AdminLTE1/App_Start/RouteConfig.cs
+++ AdminLTE1/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace AdminLTE1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
