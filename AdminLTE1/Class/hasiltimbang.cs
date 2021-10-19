5fec6447258efb315bbb7bf1becfe50832840cda
RISKICOS <RISKICOS@DESKTOP-MDE8QRD>
2021-10-19 08:54:59 +0700

    UPDATE FORM

---
+++ AdminLTE1/Class/hasiltimbang.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminLTE1.Class
+{
+    public class hasiltimbang
+    {
+       
+        public string Group { get; set; }
+        public Double Hasil { get; set; }
+    }
+}
--- AdminLTE1/Controllers/ChartController.cs
+++ AdminLTE1/Controllers/ChartController.cs
@@ -1,4 +1,5 @@
-﻿using AdminLTE1.Models;
+﻿using AdminLTE1.Class;
+using AdminLTE1.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -13,6 +14,11 @@ namespace AdminLTE1.Controllers
         DB_FINGERDataContext pb_db_ctx;
         // GET: Chart
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult IndexFLOT()
         {
             return View();
         }
@@ -50,6 +56,8 @@ namespace AdminLTE1.Controllers
             return Json(iData, JsonRequestBehavior.AllowGet);
 
         }
+
+
 
 
         [HttpPost]
@@ -119,6 +127,37 @@ namespace AdminLTE1.Controllers
                 x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
                 iData.Add(x);
             }
+            //Source data returned as JSON  
+            return Json(iData, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult MonthlyChartFLOT()
+        {
+
+          
+            //Creating sample data  
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Group", System.Type.GetType("System.String"));
+            dt.Columns.Add("Kg", System.Type.GetType("System.Double"));
+
+
+            pb_db_ctx = new DB_FINGERDataContext();
+            List<VW_MONHTLY_RESULT_GROUP> iData2 = pb_db_ctx.VW_MONHTLY_RESULT_GROUPs.ToList();
+            DataRow dr = dt.NewRow();
+
+
+            foreach (VW_MONHTLY_RESULT_GROUP dts in iData2)
+            {
+                dr = dt.NewRow();
+                dr["Group"] = dts.kelompokdesc;
+                dr["Kg"] = dts.QTY;
+                dt.Rows.Add(dr);
+            }
+            //Looping and extracting each DataColumn to List<Object>  
+
+            List<DataRow> iData = new List<DataRow>(dt.Select());
             //Source data returned as JSON  
             return Json(iData, JsonRequestBehavior.AllowGet);
 
